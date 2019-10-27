8b9a5b1248502f7e65f0f769ccc215c4de8f869e
Krystian Reinke <kryscreasy@gmail.com>
2019-10-27 15:26:12 +0100

    initial

---
+++ src/main/java/com/kryscreasy/ecommercek/EcommercekApplication.java
@@ -0,0 +1,13 @@
+package com.kryscreasy.ecommercek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kryscreasy/ecommercek/EcommercekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kryscreasy.ecommercek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
